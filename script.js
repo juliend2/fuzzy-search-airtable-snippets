37fe6e69e7662ee9ea802a5ef5e08fa6ae290acf
Julien Desrosiers <juliend2@gmail.com>
2021-06-30 11:27:21 -0400

    styles, and display results

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var base = new Airtable({ apiKey: 'keyCsBLjL1cwYff47' }).base('appGe0W4WVOEGimlm
 var found = []
 const options = {
     includeScore: true,
-    keys: ['name', 'snippet', 'techs']
+    keys: ['name', 'snippet', 'techs'],
+    shouldSort: true
 }
 
 function searchSomething(fuzzySearch) {
@@ -45,6 +46,44 @@ function searchSomething(fuzzySearch) {
             const result = fuse.search(fuzzySearch)
             resolve(result)
         }
-    })
+    })    
+}
+
+function encodeHTML(str) {
+    return str.replace(/&/g, '&amp;').replace(/\</g, '&lt;')
+}
+
+function displayFoundItem(results) {
+    if (results.length === 0) {
+        alert('Nothing found.')
+    } else {
+        document.querySelector('.results').insertAdjacentHTML('beforeend', results.map(res => {
+            if (res.item.snippet) {
+                const title = encodeHTML(res.item.name)
+                const text = encodeHTML(res.item.snippet)
+                
+                const techs = res.item.techs ? res.item.techs.map(t => `<code>#${t}</code>`).join(' ') : ''
+                return `
+                <div class="result">
+                    <h3>${title}</h3>
+                    <pre><code>${text}</code></pre>
+                    <p class="tags">${techs}</p>
+                </div>
+                `
+            } else {
+                console.log('else', res)
+            }
+
+        }).join(''))
+    }
     
+}
+
+function search(form) {
+    searchSomething(form.querySelector('input').value).then(results => {
+        console.log(results)
+        displayFoundItem(results)
+        // TODO: order by score
+    })
+    return false;
 }
